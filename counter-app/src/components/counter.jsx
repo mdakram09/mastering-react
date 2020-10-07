7ad636bdd49ba891e59443f1794271a0c2cbde0a
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-08 01:51:08 +1100

    028 Rendering Classes Dynamically: The counter color (className) dynamically changes to yellow and blue based on the count value. 0 turns to yellow and non-zero turns to blue.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,11 +7,17 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCounter()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCounter() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
