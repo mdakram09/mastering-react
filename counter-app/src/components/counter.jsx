36bbebe0542328024540ad43b59543b7c335a406
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-08 02:56:02 +1100

    030 Conditional Rendering: list tags are listed conditionally. If the tags array is empty, a message will appear.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,19 +3,26 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag."}
+        {this.renderTags()}
       </React.Fragment>
+    );
+  }
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
   getBadgeClasses() {
