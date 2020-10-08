faea97a7e0f4aecb33a379a0c7d98e82d03dfa5b
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-09 03:01:51 +1100

    044-Passing data to Components: the initial value of the counters passed through attributes as the props of the counter component to be used as the starting points.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.value,
+    // tags: ["tag1", "tag2", "tag3"],
   };
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
   render() {
+    console.log("props", this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
@@ -38,19 +39,19 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCounter() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 
   handleIncrement = (product) => {
     console.log("Increment Clicked!", this);
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 }
 
