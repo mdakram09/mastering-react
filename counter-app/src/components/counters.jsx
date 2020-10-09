a167054e946a63770bf5dbf2865d6afc562879f5
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-10 00:57:18 +1100

    050-Single Source of Truth- a reset button created. The button reset the state.counter.value to zero but because the state is local, the numbers are not changed to zero.

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -10,7 +10,13 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
-
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
@@ -18,6 +24,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
