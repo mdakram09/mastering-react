226dbfe1c4f570d625ea20b2b5707613256fd231
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-14 14:27:38 +1100

    054-Stateless Functional Components. Since the NavBar component does not have any state or helper function, it has been changed to a stateless functional component.

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar bavbar-light bg-light">
+const NavBar = (props) => {
+    return ( 
+        <nav className="navbar bavbar-light bg-light">
                 <a className="navbar-brand" href="#">
-        Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+        Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
                 </a>
             </nav>
-         );
-    }
+     );
 }
  
 export default NavBar;
