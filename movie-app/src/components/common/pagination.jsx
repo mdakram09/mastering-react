607e4ef6d763922a92d9dff9b6669d3912387edf
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-16 19:34:22 +1100

    069-Handling Page Change: the currentPage state object created to handle the current page indication in the pagination.

--- movie-app/src/components/common/pagination.jsx
+++ movie-app/src/components/common/pagination.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import _ from "lodash";
 
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, onPageChange, currentPage } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
+
+  console.log("currentPage: ", currentPage);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
   return (
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a className="page-link">{page}</a>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
           </li>
         ))}
       </ul>
--- movie-app/src/components/movies.jsx
+++ movie-app/src/components/movies.jsx
@@ -7,6 +7,7 @@ class Movies extends Component {
   state = {
     movies: getMovies(),
     pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
@@ -22,11 +23,13 @@ class Movies extends Component {
     this.setState({ movies });
   };
   handlePageChange = (page) => {
-    console.log("page");
+    // console.log(page);
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
     if (count === 0) return <p>There are no movies in the database.</p>;
     return (
       <React.Fragment>
@@ -69,8 +72,9 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
           onPageChange={this.handlePageChange}
+          currentPage={currentPage}
         />
       </React.Fragment>
     );
