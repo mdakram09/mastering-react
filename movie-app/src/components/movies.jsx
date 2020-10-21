c181e1c7d194eb6c4d8f38f795cbcc57961351e4
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-21 21:59:29 +1100

    082-Moving the Responsibility

--- movie-app/src/components/movies.jsx
+++ movie-app/src/components/movies.jsx
@@ -40,15 +40,7 @@ class Movies extends Component {
     // console.log(genre);
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
-  handleSort = (path) => {
-    console.log(path);
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -86,6 +78,7 @@ class Movies extends Component {
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
+            sortColumn={sortColumn}
           />
           <Pagination
             itemsCount={filtered.length}
