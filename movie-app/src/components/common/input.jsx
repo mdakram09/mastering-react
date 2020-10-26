d93fb5413d321b2730f9dece8a746cca7b2c7d1d
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-26 17:16:39 +1100

    129-Extracting Helper Rendering Methods

--- movie-app/src/components/common/input.jsx
+++ movie-app/src/components/common/input.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-const Input = ({ name, label, value, onChange, error }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        name={name}
-        autoFocus
-        id={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
--- movie-app/src/components/loginForm.jsx
+++ movie-app/src/components/loginForm.jsx
@@ -18,23 +18,9 @@ class LoginForm extends Form {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
