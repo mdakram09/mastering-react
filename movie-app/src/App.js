cad4d8ff1fb2d48a7619cdc777162530afc7cb25
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-27 20:02:45 +1100

    132+133: Exercies: a new button to add a new movie added on top of the movie table. also, movies information can be modified in a form.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,7 +5,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
-import MoviesForm from "./components/moviesForm";
+import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import "./App.css";
@@ -18,7 +18,7 @@ function App() {
         <Switch>
           <Route path="/register" component={RegisterForm} />
           <Route path="/login" component={LoginForm} />
-          <Route path="/movies/:id" component={MoviesForm} />
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
