9b94d25da01bac0c86be4a80eb699d0ca3aab4a6
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-29 00:29:38 +1100

    145-Expected and Unexpected Errors

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -46,7 +46,11 @@ class App extends Component {
       await axios.delete(apiEndpoint + "/" + post.id);
       // throw new Error("");
     } catch (ex) {
-      alert("Something failed while deleting a post!");
+      if (ex.response && ex.response.status === 404)
+        alert("This post has already been deleted.");
+      else {
+        console.log("Logging the error: ", ex);
+      }
       this.setState({ posts: originalPosts });
     }
   };
