65648881825387896493140dfd1ebdad31f2cf9e
Morris-Desktop <morteza.hazrati@gmail.com>
2020-10-29 00:58:23 +1100

    146-Axios Interceptors

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status <= 500;
+
+  if (!expectedError) {
+    console.log("Logging the error: ", error);
+    alert("An unexpected error occurred.");
+  }
+
+  return Promise.reject(error);
+});
+
 const apiEndpoint = "http://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
@@ -48,9 +63,6 @@ class App extends Component {
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         alert("This post has already been deleted.");
-      else {
-        console.log("Logging the error: ", ex);
-      }
       this.setState({ posts: originalPosts });
     }
   };
